af647a95306a707420592321f17cf2c8e3f0e2f8
Farah Jahangir <141430739+farahjahangir12@users.noreply.github.com>
2024-08-13 05:45:36 -0700

    Update Paper.jsx

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 
 function Paper() {
   const [query, setQuery] = useState("Tech");
-  const { papers, loading, error } = useFetch(query, 5);
+  const { papers, loading, error } = useFetch(query,5);
 
   const handleSearch = (searchQuery) => {
     setQuery(searchQuery);
@@ -17,13 +17,13 @@ function Paper() {
       </div>
       {loading && <div>Loading Papers...</div>}
       {error && <div>Error: {error}</div>}
-      <div className="mt-4 p-4">
+      <div className="mt-4 p-4 flex jutsify-center">
         {papers.map((paper) => (
           <div key={paper.paperId} className=" border border-[#45171d] p-4 rounded-lg mb-4 p-4">
             <h2 className="font-bold text-3xl mb-4 text-[#fecea8]">{paper.title}</h2>
             <h5 className="font-bold text-xl mb-2 text-[#fecea8]">Abstract</h5>
             <p className="text-white">{paper.abstract}||{paper.title}</p>
-            <div className="flex gap-4 mt-3 text-[#fecea8] text-sm text-center">
+            <div className="flex flex-col md:flex-row gap-3 mt-3 text-[#fecea8] text-sm text-center">
             <p className="rounded-full p-2 md:p-4 bg-[#e46161]"><strong>Authors:</strong> {paper.authors.map(author => author.name).join(', ')}</p>
             <p className="rounded-full p-2 md:p-4 bg-[#e46161]"><strong>Year:</strong> {paper.year}</p>
             <p className="rounded-full p-2 md:p-4 bg-[#e46161]"><strong>Citations:</strong> {paper.citationCount}</p>
