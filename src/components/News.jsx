fc2688d7eb5957bb1abc19af9855ebdb96bcb90a
Farah Jahangir <141430739+farahjahangir12@users.noreply.github.com>
2024-08-18 03:00:59 -0700

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,19 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Cards from './Cards';
 
+const API_KEY="pub_511492ba52de802f00707146b1acc558e54c6"
+
 function News() {
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
         const fetchNews = async () => {
             try {
-                const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us', {
-                    headers: {
-                        'Authorization': `Bearer pub_511492ba52de802f00707146b1acc558e54c6`,
-                        'Content-Type': 'application/json'
-                    }
-                });
-                const articles = response.data.articles;
+                const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${API_KEY}&q=technology&country=us&language=en`
+                   );
+                const articles = response.data.results;
                 setArticles(articles);
             } catch (error) {
                 console.error("Error while fetching news:", error);
@@ -24,7 +22,7 @@ function News() {
         fetchNews();
     }, []);
 
-    const titled = articles.filter((article) => article.title && article.url !== 'Null');
+    const titled = articles.filter((article) => article.title && article.link !== 'Null');
 
     return (
         <div className="flex flex-row flex-wrap gap-4 justify-center mt-4">
@@ -32,9 +30,9 @@ function News() {
                 titled.map((element) => (
                     <Cards 
                         key={element.url} 
-                        image={element.urlToImage} 
+                        image={element.image_url} 
                         title={element.title}  
-                        content={element.url} 
+                        content={element.link} 
                         className="cards" 
                     />
                 ))
