59d28863af6cb01a01d86c47284eb81ffd3f01bd
Farah Jahangir <141430739+farahjahangir12@users.noreply.github.com>
2024-08-18 03:06:26 -0700

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,16 +2,17 @@ import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Cards from './Cards';
+import Image from '../assets/pexels-pixabay-414860.jpg';
 
 const API_KEY="pub_511492ba52de802f00707146b1acc558e54c6"
 
-function News() {
+function News([topics]) {
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
         const fetchNews = async () => {
             try {
-                const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${API_KEY}&q=technology&country=us&language=en`
+                const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${API_KEY}&q=${topics}&language=en`
                    );
                 const articles = response.data.results;
                 setArticles(articles);
@@ -22,7 +23,7 @@ function News() {
         fetchNews();
     }, []);
 
-    const titled = articles.filter((article) => article.title && article.link !== 'Null');
+    const titled = articles.filter((article) => article.title && article.link!=='Null');
 
     return (
         <div className="flex flex-row flex-wrap gap-4 justify-center mt-4">
@@ -30,7 +31,7 @@ function News() {
                 titled.map((element) => (
                     <Cards 
                         key={element.url} 
-                        image={element.image_url} 
+                        image={element.image_url || Image} 
                         title={element.title}  
                         content={element.link} 
                         className="cards" 
